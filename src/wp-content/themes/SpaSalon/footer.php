11101cde3e63e22bdfdf23b527c8d7bfb1c559e2
Chris Keavey <chris@chriskeavey.com>
2017-02-10 23:39:03 +0000

    Commit of, um, everything...
    New project anticipated so this commit brings this Wordpress repo up to
    date.

--- src/wp-content/themes/SpaSalon/footer.php
+++ src/wp-content/themes/SpaSalon/footer.php
@@ -12,7 +12,6 @@ if ( is_home() ) { ?>
 
     	<div class="footer_in Page 2 column - Left Sidebar">
     <p class="copyright ">&copy; <?php echo date("Y");?> <?php bloginfo('name'); ?>. All Rights Reserved. </p>
-    <p class="credits">Designed by <a href="http://bstech.co.uk" title="BSTECH" alt="wordpress themes">Wordpress themes</a></p>
   </div>
 
    <?php } else {
@@ -20,7 +19,6 @@ if ( is_home() ) { ?>
 
  <div class="footer_in <?php if(get_option('ptthemes_page_layout')) { ?> <?php echo get_option('ptthemes_page_layout'); ?> <?php } ?>">
     <p class="copyright ">&copy; <?php echo date("Y");?>  <?php bloginfo('name'); ?>. All Rights Reserved. </p>
-    <p class="credits">Designed by <a href="http://bstech.co.uk" title="Wordpress themes" alt="wordpress themes">Wordpress themes</a></p>
   </div>
 
  <?php }
